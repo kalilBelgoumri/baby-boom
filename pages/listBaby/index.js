1f77ac452ec126d9b8215d042e8c986c5cfd2762
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-02-02 15:45:45 +0100

    update

--- pages/listBaby/index.js
+++ pages/listBaby/index.js
@@ -1,33 +1,56 @@
 import Layout from "../../components/Layout";
 import Head from "next/head";
+import CardMedia from "../../components/CardMedia";
+import Router from "next/router";
+import UsersResults from "../../components/UsersResults";
 
-export default function Baby() {
+export default function Baby({ results }) {
   return (
     <>
       <Layout page="Search result">
         <Head>
           <title>List Baby</title>
         </Head>
-        <h1 className="text-center mt-5">List Baby Search</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius atque,
-          ratione voluptatibus reiciendis obcaecati beatae iusto ex explicabo
-          quos ipsum vel sapiente labore corrupti assumenda, tempore fuga
-          repellat? Molestiae, reprehenderit.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius atque,
-          ratione voluptatibus reiciendis obcaecati beatae iusto ex explicabo
-          quos ipsum vel sapiente labore corrupti assumenda, tempore fuga
-          repellat? Molestiae, reprehenderit.
-        </p>{" "}
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius atque,
-          ratione voluptatibus reiciendis obcaecati beatae iusto ex explicabo
-          quos ipsum vel sapiente labore corrupti assumenda, tempore fuga
-          repellat? Molestiae, reprehenderit.
-        </p>
+        <h1 className="text-center mt-5">344 Résultas</h1>
+        <ul className="flex flex-col items-center justify-center gap-5">
+          {results.results.map((result) => {
+            console.log(result);
+            return (
+              <>
+                <li className="text-center" key={result.id}>
+                  <UsersResults
+                    className="w-screen px-10 bg-red-800"
+                    image={result.picture.large}
+                    typo={result.name.last}
+                    typo={result.name.first}
+                    typoCity={result.location.city}
+                  />
+                  {/* <CardMedia
+                      className="w-full"
+                      image={result.picture.large}
+                      typo={result.name.last}
+                      typo={result.name.first}
+                      typoCity={result.location.city}
+                    /> */}
+                </li>
+              </>
+            );
+          })}
+        </ul>
       </Layout>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const useDummyData = false;
+  const data = await fetch(
+    `https://randomuser.me/api/?gender=female&results=10&nat=fr`
+  ).then((response) => response.json());
+  console.log(data);
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
