d67f93067abf4ef8847e79d8ebfed12ae25c9a46
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-02-14 08:50:50 +0100

    update get and post firebase

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -7,6 +7,7 @@ import "firebase/auth";
 import { data } from "autoprefixer";
 
 export default function index() {
+  const [, forceUpdate] = React.useState();
   const [name, setCustomerName] = useState("");
   const [password, setCustomerPassword] = useState("");
   const [notesArray, setNotesArray] = useState([]);
@@ -15,22 +16,44 @@ export default function index() {
 
   // Get infos from firebase
 
+  // useEffect(() => {
+  //   if (user) {
+  //     const intervalId = setInterval(() => {
+  //       db.collection("users")
+  //         .get()
+  //         .then((data) => {
+  //           setNotesArray(
+  //             data.docs.map((note) => {
+  //               return { ...note.data() };
+  //             })
+  //           );
+  //           console.log();
+  //         });
+  //     }, 6000);
+  //     return () => clearInterval(intervalId);
+  //   } else {
+  //     null;
+  //   }
+  // }, []);
+
+  const docRef = db.collection("users").doc(user.uid);
   useEffect(() => {
-    setInterval(() => {
-      user
-        ? db
-            .collection("users")
-            .get()
-            .then((data) => {
-              setNotesArray(
-                data.docs.map((note) => {
-                  return { ...note.data(), id: note.id };
-                })
-              );
-            })
-        : alert("user not logged in");
-    });
-    return () => clearInterval(); //This is important
+    setTimeout(forceUpdate, 2000);
+    docRef
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          const myData = doc.data();
+          setNotesArray(myData);
+          console.log("Document data:", doc.data());
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
   }, []);
 
   // Put infos from firebase
@@ -52,36 +75,29 @@ export default function index() {
   return (
     <div className="">
       <Dashboard />
-      <div className="App">
-        <div className="App__form">
-          <input
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setCustomerName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setCustomerPassword(e.target.value)}
-          />
-          <button onClick={submit}>Submit</button>
+      <form onSubmit={submit}>
+        <div className="App">
+          <div className="App__form">
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setCustomerName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setCustomerPassword(e.target.value)}
+            />
+            <button>Submit</button>
+          </div>
+          <li>
+            {" "}
+            {notesArray.name} {notesArray.password}{" "}
+          </li>
         </div>
-        <ul>
-          {notesArray.map((note) => {
-            return (
-              <li
-                key={note.id}
-                value={notesArray}
-                onChange={(e) => setNotesArray(e.target.value)}
-              >
-                {note.name} {note.password}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      </form>
     </div>
   );
 }
